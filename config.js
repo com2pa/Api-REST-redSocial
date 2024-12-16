9300cdde66e17b509a780d4e915a156ccc3d9b0a
Merwil Vegas <com2pa@gmail.com>
2024-12-16 08:54:55 -0400

    configuracion para la conexion local ,produccion y mongodb

---
+++ config.js
@@ -0,0 +1,14 @@
+const PAGE_URL = process.env.MONGODB_URI ==='production'
+    ? 'placeholder'
+    : 'http://localhost:5173';
+
+const MONGO_URL = process.env.NODE_ENV === 'production'
+    ? process.env.MONGO_URI_PRODUC
+    : process.env.MONGO_URI_TEST
+
+module.exports={
+    PAGE_URL,
+    MONGO_URL,
+    //  process.env.MONGODB_URI ||'mongodb://localhost:27017/mi-red_social'
+};
+
